5ca89874c43cf4342ab95032ff6aea99ea8c5684
Margo <margo.k1007@gmail.com>
2018-10-02 19:39:57 +0300

    project automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,82 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var server = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("sass", function(){
     gulp.src("sass/style.scss")
+        .pipe(plumber())
         .pipe(sass())
-        .pipe(gulp.dest("css"));
+        .pipe(postcss([
+            autoprefixer({browsers: [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+            ]}),
+            mqpacker({
+                sort: true
+            })
+        ]))
+        .pipe(gulp.dest("css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("css"))
+        .pipe(server.stream());
 });
+
+gulp.task("images", function() {
+    return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("clean", function() { 
+  return del("build"); 
+});
+
+gulp.task("copy", function(){
+    return gulp.src([
+        "fonts/*.ttf",
+        "img/**",
+        "js/**",
+        "*.html",
+        "css/style.min.css"
+    ], {
+        base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("serve", ["style"], function() {
+    server.init({
+        server: "." 
+    });
+
+    gulp.watch("less/**/*.less", ["style"]);
+    gulp.watch("*.html")
+        .on("change", server.reload);
+});
+
+gulp.task("build", function(fn) {
+    run("clean", "copy", "sass", "images", fn);
+});
+
+
+
+
+
+
+
